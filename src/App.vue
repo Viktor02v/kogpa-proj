1be4577ac48a21c445c10308cef8a9d08bc7df72
Viktor Petruk <damnnn261@gmail.com>
2024-09-18 20:41:28 +0300

    make a try

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,30 @@
 </script>
 
 <template>
-	<div class="sm:text-3xl">Hey there</div>
-	<div class=" ">Hey  there there</div>
+	<div class="md:flex md:flex-row md:px-10 flex flex-col items-center gap-10 my-10 w-full h-full">
+		<div class="flex flex-col h-[1000px] w-[calc(100%-10%)] md:w-full md:h-[200px] md:gap-2 md:grid md:grid-cols-3">
+			<div class="flex border h-full text-white text-center rounded-lg p-4 bg-black">
+				<div class="flex justify-center mx-auto items-center">Hey</div>
+			</div>
+			<div class="flex border h-full text-white text-center rounded-lg p-4 bg-black">
+				<div class="flex justify-center mx-auto items-center">There</div>
+			</div>
+			<div class="flex border h-full text-white text-center rounded-lg p-4 bg-black">
+				<div class="flex justify-center mx-auto items-center">How</div>
+			</div>
+			<div class="flex border h-full text-white text-center rounded-lg p-4 bg-black">
+				<div class="flex justify-center mx-auto items-center">Are</div>
+			</div>
+			<div class="flex border h-full text-white text-center rounded-lg p-4 bg-black">
+				<div class="flex justify-center mx-auto items-center">You</div>
+			</div>
+		</div>
+
+		<div class="h-[500px] w-[calc(100%-10%)] md:h-[200px] md:w-[500px] ">
+			<div class="border h-full rounded-lg p-4 bg-black">
+			</div>
+		</div>
+	</div>
 </template>
 
-<style>
-</style>
+<style></style>
