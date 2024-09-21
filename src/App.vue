0d50aa8b17a2d6d540cf23028a0c45e72c3c8064
Viktor Petruk <damnnn261@gmail.com>
2024-09-21 11:39:59 +0300

    check

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,19 @@
 <script setup>
+import Menu from 'vue-material-design-icons/Menu.vue';
 </script>
 
 <template>
-	<div class="md:flex md:flex-row md:px-10 flex flex-col items-center gap-10 my-10 w-full h-full">
-		<div class="flex flex-col h-[1000px] w-[calc(100%-10%)] md:w-full md:h-[200px] md:gap-2 md:grid md:grid-cols-3">
-			<div class="flex border h-full text-white text-center rounded-lg p-4 bg-black">
-				<div class="flex justify-center mx-auto items-center">Hey</div>
-			</div>
-			<div class="flex border h-full text-white text-center rounded-lg p-4 bg-black">
-				<div class="flex justify-center mx-auto items-center">There</div>
-			</div>
-			<div class="flex border h-full text-white text-center rounded-lg p-4 bg-black">
-				<div class="flex justify-center mx-auto items-center">How</div>
-			</div>
-			<div class="flex border h-full text-white text-center rounded-lg p-4 bg-black">
-				<div class="flex justify-center mx-auto items-center">Are</div>
-			</div>
-			<div class="flex border h-full text-white text-center rounded-lg p-4 bg-black">
-				<div class="flex justify-center mx-auto items-center">You</div>
-			</div>
-		</div>
-
-		<div class="h-[500px] w-[calc(100%-10%)] md:h-[200px] md:w-[500px] ">
-			<div class="border h-full rounded-lg p-4 bg-black">
-			</div>
-		</div>
-	</div>
+	<!-- Header:start -->
+	<header id="header" class="header">
+		<nav id="navigation">
+			<ul class="menu md:hidden md:flex">
+				<li class="menu-item"><img width="140" src="/icons/kogpa-logo.png" alt="logo"></li>
+				<li class="menu-item">
+					<Menu />
+				</li>
+			</ul>
+		</nav>
+	</header>
 </template>
 
 <style></style>
